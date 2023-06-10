89b2aa86de9b47f1d900188aa342e94a407095fd
Joffre <joffre_quintas@hotmail.com>
2023-06-10 08:37:43 -0300

    ESLINT instanciado

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ import app from './app';
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log(`Servidor rodando na porta ${port}`))
+app.listen(port, () => console.log(`Servidor rodando na porta ${port}`));
