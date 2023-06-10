d96a7789159a996a2b2b00650865ec5da53a7e47
Joffre <joffre_quintas@hotmail.com>
2023-06-10 08:51:35 -0300

    Conexão com o banco estabelecida.

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,1 +1,15 @@
 import mongoose from 'mongoose';
+import 'dotenv/config';
+
+const user = process.env.DB_USER;
+const password = process.env.DB_PASSWORD;
+
+export default async function startDB() {
+    try {
+        await mongoose.connect(`mongodb+srv://${user}:${password}@joffre.ckgez7n.mongodb.net/`);
+        console.log('Banco conectado!');       
+    } catch (err) {
+        console.error(err);
+    }
+}
+
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import app from './app';
+import startDB from './database/db';
 
 const port = process.env.PORT || 3000;
+startDB();
 
 app.listen(port, () => console.log(`Servidor rodando na porta ${port}`));
