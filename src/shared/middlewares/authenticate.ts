bbee333059c872d4c601c449e6907e5b51cabb13
Joffre <joffre_quintas@hotmail.com>
2023-06-13 10:06:01 -0300

    Finalização do authenticate.

--- src/shared/middlewares/authenticate.ts
+++ src/shared/middlewares/authenticate.ts
@@ -1,11 +1,19 @@
 import { Request, Response, NextFunction } from 'express';
 import { StatusCodes } from 'http-status-codes';
+import jwt from 'jsonwebtoken';
 
-export default function authenticate(req: Request,res: Response, next:NextFunction) {
-    const auth = req.headers.token;
+export default async function authenticate(req: Request,res: Response, next:NextFunction) {
+    const auth = req.headers.token as string;
+    const secret = process.env.SECRET as string;
+
     console.log(auth)
     if(!auth){
         return res.status(StatusCodes.NOT_FOUND).json({msg: 'Necessário realizar o login.'})
     }
-    next()
+    try {
+        const validToken = await jwt.verify(auth, secret ) 
+        next()        
+    } catch (err) {
+        return res.status(StatusCodes.UNAUTHORIZED).json({msg: 'Token inválido!'})
+    }
 }
