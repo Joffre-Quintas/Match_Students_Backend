003b0d51d4587a8b4085f78fcb847033066077c5
Joffre <joffre_quintas@hotmail.com>
2023-06-22 16:43:16 -0300

    Compilado os arquivos com tsc

--- dist/database/db.js
+++ dist/database/db.js
@@ -1,13 +1,4 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -16,15 +7,13 @@ const mongoose_1 = __importDefault(require("mongoose"));
 require("dotenv/config");
 const user = process.env.DB_USER;
 const password = process.env.DB_PASSWORD;
-function startDB() {
-    return __awaiter(this, void 0, void 0, function* () {
-        try {
-            yield mongoose_1.default.connect(`mongodb+srv://${user}:${password}@joffre.ckgez7n.mongodb.net/`);
-            console.log('Banco conectado!');
-        }
-        catch (err) {
-            console.error(err);
-        }
-    });
+async function startDB() {
+    try {
+        await mongoose_1.default.connect(`mongodb+srv://${user}:${password}@joffre.ckgez7n.mongodb.net/`);
+        console.log('Banco conectado!');
+    }
+    catch (err) {
+        console.error(err);
+    }
 }
 exports.default = startDB;
